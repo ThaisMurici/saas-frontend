a3cd02ad28ee2fd3ca4d99788ae58ab7a9c869e1
Thais Moraes <murici.thais@gmail.com>
2019-05-21 11:38:37 -0300

    Add TeamSwitcher component layout

---
+++ src/components/TeamSwitcher/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import { Container, TeamList, Team } from './styles';
+
+export default class TeamSwitcher extends Component {
+  render() {
+    return (
+      <Container>
+        <TeamList>
+          <Team>
+            <img
+              src="https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=Rocketseat"
+              alt="Rocketseat"
+            />
+          </Team>
+          <Team>
+            <img
+              src="https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=Rocketseat"
+              alt="Rocketseat"
+            />
+          </Team>
+          <Team>
+            <img
+              src="https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=Rocketseat"
+              alt="Rocketseat"
+            />
+          </Team>
+          <Team>
+            <img
+              src="https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=Rocketseat"
+              alt="Rocketseat"
+            />
+          </Team>
+        </TeamList>
+      </Container>
+    );
+  }
+}
