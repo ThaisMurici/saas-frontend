b882a899c70c60a587beee20bc4b30f3a80e32c4
Thais Moraes <murici.thais@gmail.com>
2019-05-23 11:34:42 -0300

    Add sign out feature

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,7 @@
 import { all, takeLatest } from 'redux-saga/effects';
 
 import { getTeams, createTeam } from './teams';
-import { signIn } from './auth';
+import { signIn, signOut } from './auth';
 
 import { AuthTypes } from '../ducks/auth';
 import { TeamsTypes } from '../ducks/teams';
@@ -9,6 +9,8 @@ import { TeamsTypes } from '../ducks/teams';
 export default function* rootSaga() {
   return yield all([
     takeLatest(AuthTypes.SIGN_IN_REQUEST, signIn),
+    takeLatest(AuthTypes.SIGN_OUT, signOut),
+
     takeLatest(TeamsTypes.GET_TEAMS_REQUEST, getTeams),
     takeLatest(TeamsTypes.CREATE_TEAM_REQUEST, createTeam),
   ]);
